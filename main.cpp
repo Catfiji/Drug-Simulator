b2a7840696c0cd0a493265bb53d5625328d66023
Cathonia <68382607+Catfiji@users.noreply.github.com>
2021-01-25 18:27:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,65 +11,87 @@
 #define KWHT  "\x1B[37m"
 
 // C++ but i stole everything from C and just added a class
-void print_inv();
-void shop();
+    void print_inv()
+;   void shop()
+;
 
 // location class
-class location {
-    public:
-        int prices[4];
+class location 
+{   public
+:       int prices[4];
 };
 
 // player
-class player {
-    public:
-        int money;
-        int health;
-        location location;
-        int inventory[10];
+class player 
+{   public
+:       int money
+;       int health
+;       location location
+;       int inventory[10]
+;
 };
 
 // variabls
-location downtown {{80, 150, 10}}; // prices{weed, cocaine, percocet}
-player pla {0, 100, downtown, {0, 0, 0}}; // money, health, location, inventory
+
+    // prices{weed, cocaine, percocet}
+    location downtown {{80, 150, 10}}
+;   // money, health, location, inventory
+    player pla {0, 100, downtown, {0, 0, 0}}
+;
 
 // main
-int main() {
-    print_inv();
-    printf("%s [1] Buy drugs \n",KNRM);
-
-    int inp = getchar();
-    int option1 = 49;
-
-    if (inp == option1){
-        shop(); // shop
-    } else {
-        printf("hello");
-    }
-
-    return 0;
+int main() 
+{   print_inv()
+;   printf("%s [1] Buy drugs \n",KNRM)
+;   int inp = getchar()
+;   int option1 = 49
+;   if (inp == option1)
+{       shop()
+;
+}   else 
+{       printf("hello")
+;
+}
+    return 0
+;
 }
 
 //shop
-void shop(){
-    printf("[1] Weed price: %d\n", pla.location.prices[0]);
-    printf("[2] Cocaine price: %d\n", pla.location.prices[1]);
-    printf("[3] Percocet price: %d\n", pla.location.prices[2]);
-
+void shop()
+{   printf("[1] Weed price: %d\n", pla.location.prices[0])
+;   printf("[2] Cocaine price: %d\n", pla.location.prices[1])
+;   printf("[3] Percocet price: %d\n", pla.location.prices[2])
+;
 }
 
 // print menu
-void print_inv(){
-    printf("⊱ ────── {⋅.   Drug sim   .⋅} ────── ⊰\n");
-    printf("%s                ❤  %d\n", KRED,pla.health);
-    printf("%s                💲  %d\n\n",KGRN, pla.money);
-    printf("%s              𝕴𝖓𝖛𝖊𝖓𝖙𝖔𝖗𝖞\n",KYEL);
-    printf("%s          ╔═══════════════╗\n\n",KCYN);
-    printf("%s            🌿  Weed: %d\n",KGRN, pla.inventory[0]);
-    printf("%s            🧂  Cocaine: %d\n",KWHT, pla.inventory[1]);
-    printf("%s            💊  Percocet: %d\n\n",KMAG, pla.inventory[2]);
-    printf("%s          ╚═══════════════╝\n",KCYN);
-    printf("%s⊱ ────── {⋅. Made by Cat .⋅} ────── ⊰\n",KWHT);
-    printf(".❤ thanks for playing ❤.\n");
+// print a menu
+void print_inv()
+{   printf("⊱ ────── {⋅.   Drug sim   .⋅} ────── ⊰\n")
+;   printf("%s                ❤  %d\n", KRED, pla.health)
+;   printf("%s                💲  %d\n\n",KGRN, pla.money)
+;   printf("%s              𝕴𝖓𝖛𝖊𝖓𝖙𝖔𝖗𝖞\n",KYEL)
+;   printf("%s          ╔═══════════════╗\n\n",KCYN)
+;   printf("%s            🌿  Weed: %d\n",KGRN, p_inv[0])
+;   printf("%s            🧂  Cocaine: %d\n",KWHT, p_inv[1])
+;   printf("%s            💊  Percocet: %d\n\n",KMAG, p_inv[2])
+;   printf("%s          ╚═══════════════╝\n",KCYN)
+;   printf("%s⊱ ────── {⋅. Made by Cat .⋅} ────── ⊰\n",KWHT)
+;   printf(".❤ thanks for playing ❤.\n")
+;
+}
 
+void bank() 
+{   printf("Bank\n")
+;   printf("    ▟▉▉▉▙\n")
+;   printf("  ▟▉▉▉▉▉▉▉▙\n")
+;   printf("▟▉▉▉▉▉▉▉▉▉▉▉▙\n")
+;   printf("▛▛▛▔▔▔▔▔▔▔▜▜▜ \n")
+;   printf("▌▌▌  ▓  ▓  ▌▌▌\n")
+;   printf("▙▙▙▁▞☰☰☰▚▁▟▟▟\n")
+;   printf("       ▖▖\n")
+;   printf("    █▖        () \n")
+;   printf("      ▖█       ▏  \n")
+;   printf("     █▖     ▚▚▜▛▞▞\n")
+;
 }
