2ee356e18c2d486282eb9e439904d04e632fe35e
Cathonia <68382607+Catfiji@users.noreply.github.com>
2021-01-25 02:57:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+KNRM = "\x1B[0m"
+KRED = "\x1B[31m"
+KGRN = "\x1B[32m"
+KYEL = "\x1B[33m"
+KBLU = "\x1B[34m"
+KMAG = "\x1B[35m"
+KCYN = "\x1B[36m"
+KWHT = "\x1B[37m"
+player_health = 100
+player_money = 0
+p_inv = [0,0,0]
+def print_inv():
+    print("⊱ ────── ⋅.   Drug sim   .⋅ ────── ⊰");
+    print(f"{KRED}                ❤  {player_health}")
+    print(f"{KGRN}                💲  {player_money}\n")
+    print(f"{KYEL}              𝕴𝖓𝖛𝖊𝖓𝖙𝖔𝖗𝖞")
+    print(f"{KCYN}          ╔═══════════════╗\n")
+    print(f"{KGRN}            🌿  Weed: {p_inv[0]}")
+    print(f"{KWHT}            🧂  Cocaine: {p_inv[1]}")
+    print(f"{KMAG}            💊  Percocet: {p_inv[2]}\n")
+    print(f"{KCYN}          ╚═══════════════╝",KCYN)
+    print(f"{KWHT}⊱ ────── ⋅. Made by Cat .⋅ ────── ⊰",KWHT)
+	print(".  ❤ thanks for playing ❤  .\n")
+def shop():
+    print("hello")
+def main():
+    inp = input()
+    if inp == "1":
+        print_inv()
+    else:
+        print("hello")
+main()
