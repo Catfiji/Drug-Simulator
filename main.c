83cdf99c9883dd7657fce982183e28f21ee42386
Cathonia <68382607+Catfiji@users.noreply.github.com>
2021-01-25 18:20:27 -0500

    Update main.c

--- main.c
+++ main.c
@@ -12,84 +12,87 @@
 #define KWHT  "\x1B[37m"
 
 // player
-struct player{
-    unsigned int money;
-    unsigned int health;
-    unsigned int location[];
-    }; 
+struct player
+{   unsigned int money
+;   unsigned int health
+;   unsigned int location[]
+;
+}; 
 
 
 // stop minimizing code, you fucking insane asylum patient
-int p_inv[] = {0, 0, 0};
-int city[] = {100, 200, 11};
-int downtown[] = {80, 150, 10};  // replace with locations array that has an array of locations that have arrays of prices
-void print_inv(void); 
-void shop(void);
-void bank(void);
-struct player pla = {0, 100, 0}; // Money, Health
+    int p_inv[] = {0, 0, 0}
+;   int city[] = {100, 200, 11}
+;   int downtown[] = {80, 150, 10} // replace with locations array that has an array of locations that have arrays of prices
+;   void print_inv(void)
+;   void shop(void)
+;   struct player pla = {0, 100, 0} // Money, Health, Location
+;   void bank(void)
+;
 
 
 // main
-int main(void) {
-    print_inv(); // moved up
-    printf("%s [1] Buy drugs \n",KNRM); // temp-menu 
-    int inp = getchar();
-    int option1 = 49; // idk how i got this value just roll with it, it means "1" though haha
-
-    if (inp == option1){
-        shop();
-    } else {
-        printf("hello");
-    }
-    
-    return 0;
+int main(void) 
+{   print_inv()
+;   printf("%s [1] Buy drugs \n",KNRM) // temp-menu 
+;   int inp = getchar()
+;   int option1 = 49
+;   if (inp == option1)
+{       shop()
+;
+}   else 
+{       printf("hello")
+;
+}   return 0
+;
 }
 
 // shop WORKING ON
-void shop(void){
-    int location[5];
-
-    if (pla.location[0] == 0) {
-        location[1] = downtown[0]; // drugs start at array 1
-        location[2] = downtown[1];
-        location[3] = downtown[2];
-    } else if (pla.location[0] == 1) {
-        location[1] = city[0];
-        location[2] = city[1];
-        location[3] = city[2];
-    }
-
-    printf("[1] Weed price: %d\n", location[1]);
-    printf("[2] Cocaine price: %d\n", location[2]);
-    printf("[3] Percocet price: %d\n", location[3]);
-
+void shop(void)
+{   int location[5]
+;	if (pla.location[0] == 0) 
+{       location[1] = downtown[0] // drugs start at array 1
+;       location[2] = downtown[1]
+;       location[3] = downtown[2]
+;
+}   else if (pla.location[0] == 1) 
+{       location[1] = city[0]
+;       location[2] = city[1]
+;       location[3] = city[2]
+;
+}   printf("[1] Weed price: %d\n", location[1])
+;   printf("[2] Cocaine price: %d\n", location[2])
+;   printf("[3] Percocet price: %d\n", location[3])
+;
 }
 
 // print a menu
-void print_inv(void){
-    printf("⊱ ────── {⋅.   Drug sim   .⋅} ────── ⊰\n");
-    printf("%s                ❤  %d\n", KRED, pla.health);
-    printf("%s                💲  %d\n\n",KGRN, pla.money);
-    printf("%s              𝕴𝖓𝖛𝖊𝖓𝖙𝖔𝖗𝖞\n",KYEL);
-    printf("%s          ╔═══════════════╗\n\n",KCYN);
-    printf("%s            🌿  Weed: %d\n",KGRN, p_inv[0]);
-    printf("%s            🧂  Cocaine: %d\n",KWHT, p_inv[1]);
-    printf("%s            💊  Percocet: %d\n\n",KMAG, p_inv[2]);
-    printf("%s          ╚═══════════════╝\n",KCYN);
-    printf("%s⊱ ────── {⋅. Made by Cat .⋅} ────── ⊰\n",KWHT);
-    printf(".❤ thanks for playing ❤.\n"); // cute but remove this line
+void print_inv(void)
+{   printf("⊱ ────── {⋅.   Drug sim   .⋅} ────── ⊰\n")
+;   printf("%s                ❤  %d\n", KRED, pla.health)
+;   printf("%s                💲  %d\n\n",KGRN, pla.money)
+;   printf("%s              𝕴𝖓𝖛𝖊𝖓𝖙𝖔𝖗𝖞\n",KYEL)
+;   printf("%s          ╔═══════════════╗\n\n",KCYN)
+;   printf("%s            🌿  Weed: %d\n",KGRN, p_inv[0])
+;   printf("%s            🧂  Cocaine: %d\n",KWHT, p_inv[1])
+;   printf("%s            💊  Percocet: %d\n\n",KMAG, p_inv[2])
+;   printf("%s          ╚═══════════════╝\n",KCYN)
+;   printf("%s⊱ ────── {⋅. Made by Cat .⋅} ────── ⊰\n",KWHT)
+;   printf(".❤ thanks for playing ❤.\n")
+;
 }
 
-void bank(void) {
-    printf("Bank\n");
-    printf("    ▟▉▉▉▙\n");
-    printf("  ▟▉▉▉▉▉▉▉▙\n");
-    printf("▟▉▉▉▉▉▉▉▉▉▉▉▙\n");
-    printf("▛▛▛▔▔▔▔▔▔▔▜▜▜ \n");
-    printf("▌▌▌  ▓  ▓  ▌▌▌\n");
-    printf("▙▙▙▁▞☰☰☰▚▁▟▟▟\n");
-    printf("       ▖▖\n");
-    printf("    █▖        () \n");
-    printf("      ▖█       ▏  \n");
-    printf("     █▖     ▚▚▜▛▞▞\n");
+void bank(void) 
+{   printf("Bank\n")
+;   printf("    ▟▉▉▉▙\n")
+;   printf("  ▟▉▉▉▉▉▉▉▙\n")
+;   printf("▟▉▉▉▉▉▉▉▉▉▉▉▙\n")
+;   printf("▛▛▛▔▔▔▔▔▔▔▜▜▜ \n")
+;   printf("▌▌▌  ▓  ▓  ▌▌▌\n")
+;   printf("▙▙▙▁▞☰☰☰▚▁▟▟▟\n")
+;   printf("       ▖▖\n")
+;   printf("    █▖        () \n")
+;   printf("      ▖█       ▏  \n")
+;   printf("     █▖     ▚▚▜▛▞▞\n")
+;
 }
